f83d86d09f3b4073262c7dac63ee417452365a86
Javad-mun <skhataeipour@mun.ca>
2019-05-23 11:34:29 -0230

    Eddited [ackage help page.

--- R/activityCounts-package.R
+++ R/activityCounts-package.R
@@ -1,7 +1,23 @@
 #' @keywords internal
 #' @author Ruben Brondeel, Javad Rahimipour Anaraki, SeyedJavad KhataeiPour, Daniel Fuller.
 #'
-#' \subsection{Maintainers}{SeyedJavad KhataeiPour <skhataeipour@mun.ca>, Javad Rahimipour Anaraki <jra066@mun.ca>, Daniel Fuller <dfuller@mun.ca>}
+#' \subsection{Maintainers}{SeyedJavad KhataeiPour <skhataeipour@mun.ca>,
+#'  Javad Rahimipour Anaraki <jra066@mun.ca>, Daniel Fuller <dfuller@mun.ca>
+#'  }
+#' @details
+#' There are two datasets and a main function in this package
+#' @family The main function anf sample datasets
+#' @seealso \link{counts}
+#'  to see how to produce counts.
+#'
+#'  \link{sampleXYZ}
+#'  raw accelerometer data for testing \code{counts()} function.
+#'
+#'  \link{activityCounts}
+#'  raw accelerometer data for testing \code{counts()} function.
+#'
+#'
+#'
 "_PACKAGE"
 
 
