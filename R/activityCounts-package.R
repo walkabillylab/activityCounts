ed38fa42df2f074f240cb2fcfbc7202a9ee77db1
Javad-mun <skhataeipour@mun.ca>
2019-05-23 11:03:07 -0230

    Ediited the package's help page.

--- R/activityCounts-package.R
+++ R/activityCounts-package.R
@@ -1,4 +1,7 @@
 #' @keywords internal
+#' @author Ruben Brondeel, Javad Rahimipour Anaraki, SeyedJavad KhataeiPour, Daniel Fuller.
+#'
+#' \subsection{Maintainers}{SeyedJavad KhataeiPour <skhataeipour@mun.ca>, Javad Rahimipour Anaraki <jra066@mun.ca>, Daniel Fuller <dfuller@mun.ca>}
 "_PACKAGE"
 
 
