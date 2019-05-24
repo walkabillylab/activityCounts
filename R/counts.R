45e712f416cffd9356cf6c77d9a70de592c9bdb6
Javad Rahimipour Anaraki <35808171+jracp@users.noreply.github.com>
2019-05-24 10:52:51 -0230

    Added warning. Edited maintainers

--- R/counts.R
+++ R/counts.R
@@ -36,55 +36,43 @@
 #'
 #'
 #'
-counts = function(data, hertz, x_axis=1 ,y_axis=2, z_axis =3) {
+counts = function(data,
+                  hertz = -1,
+                  x_axis = 1 ,
+                  y_axis = 2,
+                  z_axis = 3) {
+  if (hertz == -1) {
+    warning("Sampling frequency is not assigned! (The default value is set to 30Hz)")
+    hertz = 100
+  }
 
-  data = data[,c(x_axis,y_axis,z_axis)]
+  data = data[, c(x_axis, y_axis, z_axis)]
 
   A = c(
-    1,
-    -4.1637,
-    7.5712,
-    -7.9805,
-    5.385,
-    -2.4636,
+    1,-4.1637,
+    7.5712,-7.9805,
+    5.385,-2.4636,
     0.89238,
-    0.06361,
-    -1.3481,
-    2.4734,
-    -2.9257,
-    2.9298,
-    -2.7816,
-    2.4777,
-    -1.6847,
+    0.06361,-1.3481,
+    2.4734,-2.9257,
+    2.9298,-2.7816,
+    2.4777,-1.6847,
     0.46483,
-    0.46565,
-    -0.67312,
-    0.4162,
-    -0.13832,
+    0.46565,-0.67312,
+    0.4162,-0.13832,
     0.019852
   )
   B = c(
-    0.049109,
-    -0.12284,
-    0.14356,
-    -0.11269,
-    0.053804,
-    -0.02023,
+    0.049109,-0.12284,
+    0.14356,-0.11269,
+    0.053804,-0.02023,
     0.0063778,
-    0.018513,
-    -0.038154,
-    0.048727,
-    -0.052577,
-    0.047847,
-    -0.046015,
-    0.036283,
-    -0.012977,
-    -0.0046262,
-    0.012835,
-    -0.0093762,
-    0.0034485,
-    -0.00080972,
-    -0.00019623
+    0.018513,-0.038154,
+    0.048727,-0.052577,
+    0.047847,-0.046015,
+    0.036283,-0.012977,-0.0046262,
+    0.012835,-0.0093762,
+    0.0034485,-0.00080972,-0.00019623
   )
 
   deadband = 0.068
@@ -110,10 +98,12 @@ counts = function(data, hertz, x_axis=1 ,y_axis=2, z_axis =3) {
     B = B * gain
     fx8up = filter(B, A, datab)
     fx8 = pptrunc(fx8up[seq(1, length(fx8up), 3)], peakThreshold)
-    out = cbind(out, runsum(floor(trunc(abs(fx8), deadband) / adcResolution), integN, 0))
+    out = cbind(out, runsum(floor(trunc(
+      abs(fx8), deadband
+    ) / adcResolution), integN, 0))
 
   }
 
-  colnames(out) = c("x","y","z")
+  colnames(out) = c("x", "y", "z")
   return(out)
 }
